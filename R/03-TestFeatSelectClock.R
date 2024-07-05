21487f020e84cd13bd4f223102534b7a4ac01017
ljnewediuk <j.newedi@gmail.com>
2024-07-05 17:54:50 -0500

    Test performance and plot
    Make clocks for all subsets of sites and plot in single panel plot

--- R/03-TestFeatSelectClock.R
+++ R/03-TestFeatSelectClock.R
@@ -3,73 +3,84 @@
 
 # Shows how EWAS and aligning the genome can impact clock accuracy
 
+#  1 - Prep workspace ====
+
+# Load libraries
+library(tidyverse)
+library(cowplot)
+
 # Load clock function
 source('functions/fitClock.R')
 
 # 2 - Load data ====
 
-# Normalized betas and sample info
-meth_dat <- readRDS('output/lm_norm_betas.rds') 
+# Alignment to polar bear genome
+align <- readRDS('input/pb_alignment.rds')
+
+# Normalized betas
+meth_dat <- readRDS('input/norm_betas.rds')
 
 # Relatedness data
 sibs <- readRDS('input/full_sibs.rds')
 
-# 3 - Remove repeat samples and siblings ==== 
+# 3 - Make list of all methyl datasets ====
 
-# Check which bears have repeat samples
-multi_samps <- meth_dat %>%
-  group_by(id) %>%
-  summarize(n_samps = n()) %>%
-  filter(n_samps > 1) %>%
-  pull(id)
+# Normalized betas from ewas
+ewas_betas <- list.files('output/', pattern = 'meth_ewas')
+meth_list <- list()
+for(i in 1:length(ewas_betas)) {
+  ewas_i <- readRDS(paste0('output/', ewas_betas[i]))
+  # Add to list
+  meth_list[[i]] <- ewas_i
+  names(meth_list)[[i]] <- substr(ewas_betas[i], 1, 13)
+}
 
-# Distinct dataset with repeats and siblings removed
-distct_dat <- meth_dat %>%
-  filter(! id %in% c(sibs, multi_samps))
+# Add normalized betas aligned to genome to list
+meth_list[['meth_dat_align']] <- meth_dat %>%
+  select(c(sampleId:Population, align$qname))
 
-# Sample test data for EWAS clock
-train_dat <- distct_dat %>%
-  group_by(Population, sex, Spec, age) %>% 
-  slice_sample(n = 1)
+# Also add full set of normalized betas to list
+meth_list[['meth_dat_norm']] <- meth_dat
 
-# Training data
-test_dat <- distct_dat %>%
-  filter(! sampleId %in% train_dat$sampleId)
+# 4 - Fit clocks for each case (using methylation dfs in list) ====
 
-# Fit clock
-ewas_clock <- fitClock(train_dat, test_dat)
+plot_list <- list()
+for(i in 1:length(meth_list)) {
+  
+  # Check which bears have repeat samples
+  multi_samps <- meth_list[[i]] %>%
+    group_by(id) %>%
+    summarize(n_samps = n()) %>%
+    filter(n_samps > 1) %>%
+    pull(id)
+  
+  # Remove repeats and siblings
+  distct_dat <- meth_list[[i]] %>%
+    filter(! id %in% c(sibs, multi_samps))
+  
+  # Sample test data for feature selection clock
+  train_dat <- distct_dat %>%
+    group_by(Population, sex, Spec, age) %>% 
+    slice_sample(n = 1)
+  
+  # Training data
+  test_dat <- distct_dat %>%
+    filter(! sampleId %in% train_dat$sampleId)
+  
+  # Fit clock
+  fs_clock <- fitClock(train_dat, test_dat)
+  
+  # Add plots to list
+  plot_list[[names(meth_list)[[i]]]] <- fs_clock$plot
+  
+}
 
-ewas_clock$plot +
-  geom_jitter(aes(colour = Population))
+# 5 - Plot ====
 
-ewas_clock$data %>%
-  mutate(AgeAccel = lm(agePredict ~ age)$residuals) %>%
-  ggplot(aes(x = Population, y = AgeAccel)) + geom_boxplot()
+plot_grid(plotlist = plot_list, ncol = 3, labels = LETTERS[1:6], label_size = 20)
 
-# Fit tissue-specific clocks
-train_muscle <- distct_dat %>%
-  filter(Spec == 'Muscle') %>%
-  group_by(Population, age, sex) %>%
-  slice_sample(n = 1)
+# 6 - Save ====
 
-test_muscle <- distct_dat %>%
-  filter(Spec == 'Muscle') %>%
-  filter(! sampleId %in% train_muscle$sampleId)
-
-muscle_clock <- fitClock(train_muscle, test_muscle)
-
-muscle_clock$plot
-
-train_blood_skin <- distct_dat %>%
-  filter(Spec %in% c('Blood', 'Skin')) %>%
-  group_by(Population, age, sex) %>%
-  slice_sample(n = 1)
-
-test_blood_skin <- distct_dat %>%
-  filter(Spec %in% c('Blood', 'Skin')) %>%
-  filter(! sampleId %in% train_blood_skin$sampleId)
-
-blood_skin_clock <- fitClock(train_blood_skin, test_blood_skin)
-
-blood_skin_clock$plot
+ggsave('fig3.tiff', plot = last_plot(), 
+       device = 'tiff', path = 'figures/', dpi = 300, height = 15, width = 25, units = 'cm', bg = 'white')
 
