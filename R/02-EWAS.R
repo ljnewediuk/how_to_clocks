239f9a1c3b59047e47abef54f870d0ff7196c1ab
ljnewediuk <j.newedi@gmail.com>
2024-07-05 17:53:51 -0500

    Add EWAS
    Run EWAS for population, age, sex, tissue and then select positions correlated/uncorrelated with each

--- R/02-EWAS.R
+++ R/02-EWAS.R
@@ -4,8 +4,6 @@
 # Remove any probes correlated (p < 0.05) with tissue, sex, and population and 
 # keep top ~ 10% of probes correlated with age. 
 
-library(tidyverse)
-
 #   1) Two EWAS on age with blood and skin separated
 #   2) 11 EWAS on age with all populations separated
 #   3) Two EWAS on age with males and females separated
@@ -16,6 +14,9 @@ library(tidyverse)
 # in columns
 
 # 1 - Prep workspace ====
+
+# Load libraries
+library(tidyverse)
 
 # Load EWAS function
 source('functions/fitLimma.R')
@@ -44,6 +45,8 @@ split_sex <- meth_dat %>%
 
 # 4 - Run EWAS ====
 
+# Fit lm to test probe relationships with age, sex, tissue, and population
+# while also controlling for the other variables
 full_limma <- fitLimma(meth_dat = meth_dat)
 
 # Add adjusted pvalues for variable of interest
@@ -83,87 +86,32 @@ cgs_cor_w_age <- pvals_full %>%
   filter(if_any(everything(), ~ . < 0.05)) %>%
   pull(CGid)
 
-# 6 - Save new data ====
+# 6 - Select positions ====
 
-# Reduced Cg positions based on lm
-meth_dat_red <- meth_dat %>% 
-  select(cgs_cor_w_age) %>%
+# Reduced Cg positions based on lm (remove all correlated with sex, tissue, and
+# population and keep remaining Cgs correlated with age)
+meth_dat_full <- meth_dat %>% 
+  select(c(sampleId:Population, cgs_cor_w_age)) %>%
   select(! any_of(c(cgs_cor_w_pop, cgs_cor_w_sex, cgs_cor_w_tissue)))
 
-# Add back sample info
-new_meth_dat <- meth_dat %>%
-  select(sampleId:Population) %>%
-  bind_cols(meth_dat_red)
+# Cg positions not correlated with population, but correlated with age
+meth_dat_pop <- meth_dat %>% 
+  select(c(sampleId:Population, cgs_cor_w_age)) %>%
+  select(! any_of(cgs_cor_w_pop))
 
-saveRDS(new_meth_dat, 'output/lm_norm_betas.rds')
+# Cg positions not correlated with tissue, but correlated with age
+meth_dat_tissue <- meth_dat %>% 
+  select(c(sampleId:Population, cgs_cor_w_age)) %>%
+  select(! any_of(cgs_cor_w_tissue))
 
+# Cg positions not correlated with sex, but correlated with age
+meth_dat_sex <- meth_dat %>% 
+  select(c(sampleId:Population, cgs_cor_w_age)) %>%
+  select(! any_of(cgs_cor_w_sex))
 
+# 7 - Save ====
 
-
-
-
-
-
-
-
-
-
-
-
-# 5 Run limma for EWAS on age separately for blood and skin ====
-
-# Blood/age
-blood_age_limma <- fit_limma(betas = sample_sheet_blood, 
-                             mod_formula = c('age', 'sex'),
-                             relatives = relatives_list[[i]]) %>%
-  mutate(age_pval_adj = p.adjust(age_pval, method = 'BH')) %>%
-  mutate(sig = ifelse(age_pval < 10e-6, 'yes', 'no'))
-
-# Skin/age
-skin_age_limma <- fit_limma(betas = sample_sheet_skin, 
-                            mod_formula = c('age', 'sex'),
-                            relatives = relatives_list[[i]]) %>%
-  mutate(age_pval_adj = p.adjust(age_pval, method = 'BH')) %>%
-  mutate(sig = ifelse(age_pval < 10e-6, 'yes', 'no'))
-
-# Make table of p values correlated with age
-print(
-  data.frame(
-    tissue = c(rep('skin', times = 3), rep('blood', times = 3)),
-    pval = rep(c('10e-6', '10e-7', '10e-8'), times = 2),
-    n = c(nrow(skin_age_limma[skin_age_limma$age_pval < 10e-6,]),
-          nrow(skin_age_limma[skin_age_limma$age_pval < 10e-7,]),
-          nrow(skin_age_limma[skin_age_limma$age_pval < 10e-8,]),
-          nrow(blood_age_limma[blood_age_limma$age_pval < 10e-6,]),
-          nrow(blood_age_limma[blood_age_limma$age_pval < 10e-7,]),
-          nrow(blood_age_limma[blood_age_limma$age_pval < 10e-8,])))
-)
-
-# Use all Cgs highly correlated with age (p < 10e-6) and exclude Cgs correlated 
-# (p < 0.05) with sex
-Cgs_sample <- skin_age_limma %>%
-  # Cgs correlated with age
-  rbind(blood_age_limma) %>%
-  filter(age_pval_adj < 10e-6) %>%
-  # Remove Cgs correlated with sex and tissue
-  filter(! CGid %in% c(cgs_cor_w_sex)) %>%
-  pull(CGid)  %>% 
-  unique()
-
-# Assign Cgs as either with blocking for relatives or without
-assign(paste0('Cgs_sample_', names(relatives_list)[i]), Cgs_sample)
-
-
-
-# 6 Difference between Cgs selected with vs. without blocking by relatives ====
-
-setdiff(Cgs_sample_no_relatives, Cgs_sample_relatives)
-
-# Result:
-# 8 Cpgs different: "cg20167048" "cg05631094" "cg06074849" "cg08383062" 
-# "cg15148667" "cg16787065" "cg22661206" "cg08965235"
-# 
-# This is a ~0.2% difference from the feature set we used in the original clock,
-# and ~0.02% of all Cpgs in the array.
-
-# 7 Save ====
+saveRDS(meth_dat_full, 'output/meth_ewas_all.rds')
+saveRDS(meth_dat_pop, 'output/meth_ewas_pop.rds')
+saveRDS(meth_dat_tissue, 'output/meth_ewas_tis.rds')
+saveRDS(meth_dat_sex, 'output/meth_ewas_sex.rds')
