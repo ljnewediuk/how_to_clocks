901471f0804661754b2c56e9f42f4e7cbcbbe36f
ljnewediuk <j.newedi@gmail.com>
2024-07-19 16:38:02 -0500

    switch to functions

--- R/01-TestClockBiases.R
+++ R/01-TestClockBiases.R
@@ -10,8 +10,10 @@
 library(tidyverse)
 library(cowplot)
 
-# Load clock function
+# Load clock function and sampling function
 source('functions/fitClock.R')
+source('functions/testAccuracy.R')
+source('functions/sampleGrps2.R')
 
 # 2 - Load data ====
 
@@ -39,223 +41,186 @@ distct_dat %>%
   group_by(Population) %>%
   summarise(n())
 
-# Pick bears from 4 populations across different parts of the Arctic (2 with 
-# skin/blood and 2 with muscle) to get a good mix of tissue and geography
+# 4 - Population bias ====
 
-# Set seed for sampling
-set.seed(8)
+# Filter grps of interest
+pop_dat <- distct_dat %>%
+  filter(Population %in% c('WH', 'SB', 'NB')) %>%
+  mutate(Population = ifelse(Population %in% c('SB', 'NB'), 'BF', Population))
 
-# Sample populations, sexes, ages, tissues evenly for training data
-clock_dat <- distct_dat %>%
-  # filter(Population %in% c('WH', 'SH', 'NB', 'LS'))
-  filter(Population %in% c('WH', 'DS', 'MC', 'NB'))  %>% 
-  group_by(Population, sex, Spec, age) %>% 
-  slice_sample(n = 2)
-  
-# 4 - Unbiased clock ====
+# Function to compare accuracy across proportion overlap between Beaufort and
+# Hudson Bay population training and testing data, while drawing equal numbers 
+# of male and female samples
+pops_out <- testAccuracy(dat = pop_dat, 
+                         grp_fact = c('Population', 'sex'),
+                         grps = c('WH', 'BF', 'M', 'F'),
+                         prop_list = seq(0, 1, by = 0.1),
+                         train_overlap = F, test_overlap = F,
+                         n_times = 100, n_train = 75, n_test = 30)
 
-# Get training data for the unbiased clock
-unb_train_dat <- clock_dat %>%
-  group_by(Population, Spec, age, sex) %>%
-  slice_sample(n = 1)
+# 5 - Sex bias ====
 
-# Hold-out set is training data
-unb_test_dat <- clock_dat %>%
-  filter(! sampleId %in% unb_train_dat$sampleId)
+# Function to compare accuracy across proportion overlap male and female between 
+# training and testing data, while drawing equal numbers of samples from populations
+sex_out <- testAccuracy(dat = distct_dat, 
+                        grp_fact = c('sex', 'Population'),
+                        grps = c('F', 'M', 'WH', 'SB', 'NB'),
+                        prop_list = seq(0, 1, by = 0.1),
+                        train_overlap = F, test_overlap = F,
+                        n_times = 100, n_train = 60, n_test = 30)
 
-# Plot clock
-unb_clock <- fitClock(unb_train_dat, unb_test_dat)$plot
+# 6 - Tissue bias ====
 
-# Add label
-unb_grid <- plot_grid(unb_clock, ncol = 1, labels = 'A', label_size = 20)
+# Filter grps of interest
+tissue_dat <- distct_dat %>%
+  mutate(Tissue = ifelse(Spec %in% c('Blood', 'Skin'), 'BlSk', Spec)) %>%
+  relocate(Tissue, .after = Spec) %>%
+  filter(! Population %in% 'NW')
 
-# 5 - Population bias ====
+# Function to compare accuracy across proportion overlap tissue between training 
+# and testing data, while drawing equal numbers of samples from each population
+tissue_out <- testAccuracy(dat = tissue_dat, 
+                           grp_fact = c('Tissue', 'Population'),
+                           grps = c('BlSk', 'Muscle', unique(tissue_dat$Population)),
+                           prop_list = seq(0, 1, by = 0.1),
+                           train_overlap = T, test_overlap = T,
+                           n_times = 100, n_train = 100, n_test = 75)
 
-# Set seed for sampling
-set.seed(8)
+# 7 - Age bias ====
 
-# Sample bears from WH and MC (mix of ages, sexes, tissue types) and
-# use them to predict ages in NB and DS
+# Filter grps of interest
+age_dat <- distct_dat %>%
+  mutate(ageGrp = ifelse(age <= 5, 'Immature', 'Mature')) %>%
+  relocate(ageGrp, .after = age)
 
-# Training and testing data for population-bias (use one muscle and one skin &  
-# blood population to predict the other muscle and skin & blood population)
-# Make sure sample sizes are the same
-popb_train_dat <- clock_dat %>%
-  filter(Population %in% c('MC', 'WH')) %>%
-  ungroup() %>%
-  slice_head(n = 96)
+# Function to compare accuracy across proportion overlap immature and mature 
+# bears between training and testing data, while drawing equal numbers of 
+# samples from populations
+age_out <- testAccuracy(dat = age_dat, 
+                        grp_fact = c('ageGrp', 'Population'),
+                        grps = c('Immature', 'Mature', 'WH', 'SB', 'NB'),
+                        prop_list = seq(0, 1, by = 0.1),
+                        train_overlap = F, test_overlap = F,
+                        n_times = 100, n_train = 45, n_test = 30)
 
-popb_test_dat <- clock_dat %>%
-  filter(Population %in% c('DS', 'NB')) %>%
-  ungroup() %>%
-  slice_head(n = 87)
-
-# Training and testing data for single population (use WH to predict WH)
-pops_train_dat <- clock_dat %>%
-  filter(Population %in% c('WH', 'NB')) %>%
-  group_by(Spec, age, sex) %>%
-  slice_sample(n = 1)
-
-pops_test_dat <- clock_dat %>%
-  filter(Population %in% c('WH', 'NB') 
-         & ! sampleId %in% pops_train_dat$sampleId)
-
-# Fit both clocks
-pops_clock <- fitClock(pops_train_dat, pops_test_dat, '#ff6666')$plot
-popb_clock <- fitClock(popb_train_dat, popb_test_dat, '#2acaea')$plot
-
-# Make panel plot
-pop_grid <- plot_grid(pops_clock, popb_clock, 
-                      ncol = 1, labels = c('B', 'G'), label_size = 20)
-
-# 6 - Sex bias ====
-
-# Set seed for sampling
-set.seed(12)
-
-# Sample males from all populations and use them to predict female ages
-
-# Training and testing data for sex-bias (using females to predict males),
-# making sure sample sizes are the same
-sexb_train_dat <- clock_dat %>%
-  filter(sex == 'F') %>% 
-  ungroup() %>%
-  slice_head(n = 80)
-
-sexb_test_dat <- clock_dat %>%
-  filter(sex == 'M') %>%
-  ungroup() %>%
-  slice_head(n = 40)
-
-# Training and testing data for single sex (using females to predict females)
-sexs_train_dat <- clock_dat %>%
-  filter(sex == 'F') %>%
-  group_by(Population, Spec, age) %>%
-  slice_sample(n = 1)
-
-sexs_test_dat <- clock_dat %>%
-  filter(sex == 'F' & ! sampleId %in% sexs_train_dat$sampleId)
-
-# Fit both clocks
-sexs_clock <- fitClock(sexs_train_dat, sexs_test_dat, '#ff6666')$plot
-sexb_clock <- fitClock(sexb_train_dat, sexb_test_dat, '#2acaea')$plot
-
-# Make panel plot
-sex_grid <- plot_grid(sexs_clock, sexb_clock, 
-                      ncol = 1, labels = c('C', 'H'), label_size = 20)
-
-# 7 - Tissue bias ====
-
-# Set seed for sampling
-set.seed(4)
-
-# Sample skin samples and use them to predict remaining muscle and blood samples
-
-# Training and testing data for tissue-bias (using skin to predict muscle
-# and blood), making sure sample sizes are the same as the single-tissue test
-tisb_train_dat <- clock_dat %>%
-  filter(Spec == 'Skin')
-
-tisb_test_dat <- clock_dat %>%
-  filter(Spec %in% c('Muscle', 'Blood')) %>%
-  ungroup() %>%
-  slice_sample(n = 60)
-
-# Training and testing data for single sex (using females to predict females)
-tiss_train_dat <- clock_dat %>%
-  filter(Spec %in% c('Skin', 'Blood')) %>%
-  group_by(Population, Spec, age, sex) %>%
-  slice_sample(n = 1)
-
-tiss_test_dat <- clock_dat %>%
-  filter(Spec %in% c('Skin', 'Blood') & ! sampleId %in% tiss_train_dat$sampleId)
-
-# Fit both clocks
-tiss_clock <- fitClock(tiss_train_dat, tiss_test_dat, '#ff6666')$plot
-tisb_clock <- fitClock(tisb_train_dat, tisb_test_dat, '#2acaea')$plot
-
-# Make panel plot
-tis_grid <- plot_grid(tiss_clock, tisb_clock, 
-                      ncol = 1, labels = c('D', 'I'), label_size = 20)
-
-# 8 - Age bias ====
-
-# Set seed
-set.seed(14)
-
-# Sample bears under 10 and use them to predict bears over 10
-
-# Training and testing data for age-bias (using older bears to predict younger), 
-# making sure sample sizes are the same as the single-age group test
-ageb_train_dat <- clock_dat %>%
-  filter(age <= 10) %>%
-  ungroup() %>%
-  slice_sample(n = 71)
-
-ageb_test_dat <- clock_dat %>%
-  filter(age > 10) %>%
-  ungroup() %>%
-  slice_sample(n = 48)
-
-# Training and testing data for single age group including only bears over 10
-ages_train_dat <- clock_dat %>%
-  filter(age >= 10) %>%
-  group_by(Population, Spec, sex) %>%
-  slice_sample(n = 10)
-
-ages_test_dat <- clock_dat %>%
-  filter(age >= 10 & ! sampleId %in% ages_train_dat$sampleId)
-
-# Fit both clocks
-ages_clock <- fitClock(ages_train_dat, ages_test_dat, '#ff6666')$plot
-ageb_clock <- fitClock(ageb_train_dat, ageb_test_dat, '#2acaea')$plot
-
-# Make panel plot
-age_grid <- plot_grid(ages_clock, ageb_clock, 
-                      ncol = 1, labels = c('E', 'J'), label_size = 20)
-
-# 9 - Aging accuracy bias ====
+# 7 - Aging accuracy bias ====
 
 #  Add some error to the ages in the training samples for the unbiased clock and
-#  use those samples to predict age in the remaining test samples. The error is
-#  currently sampled from a normal distribution with mean 0 and sd between 0.3 
-#  and 3, proportional to the age of the bear (error increases with age)
+#  use those samples to predict age in the remaining test samples
 
-# Filter out only WH bears
-age_err_dat <- clock_dat %>%
+# Filter training data (only distinct WH bears)
+train_aerr_dat <- distct_dat %>%
   filter(Population == 'WH')
 
-# Sample bears by age, sex, and tissue for training set
-aerr0_train_dat <- age_err_dat %>%
-  group_by(age, sex, Spec) %>%
-  slice_sample(n = 1)
+# Filter testing data (WH bears excluded from training)
+test_aerr_dat <- meth_dat %>%
+  filter(Population == 'WH' &
+           ! sampleId %in% train_aerr_dat$sampleId) %>%
+  # Remove the duplicate sample
+  distinct()
 
-# Add some error
-aerr2_train_dat <- aerr0_train_dat %>%
-  mutate(age_orig = age,
-         age = ifelse(age > 1, age + round(rnorm(1, 0, age/5)), age)) %>%
-  relocate(age_orig, .after = age)
+# Add different degrees of error to training data and fit clock
 
-# Get test data (bears not in training data)
-aerr_test_dat  <- age_err_dat %>%
-  filter(! sampleId %in% aerr0_train_dat$sampleId)
+# Define age error with sd = 5%-25% of the average maximum lifespan of the
+# population (20 yrs) for mature bears >= 5 yrs
+aerr_seq <- seq(.05*20, .25*20, by = 1)
 
-# Fit  both clocks
-aerr0_clock <- fitClock(aerr0_train_dat, aerr_test_dat, '#ff6666')$plot
-aerr2_clock <- fitClock(aerr2_train_dat, aerr_test_dat, '#2acaea')$plot
+# Specify number of times to repeat loop
+n_times <- 100
 
-# Make panel plot
-aerr_grid <- plot_grid(aerr0_clock, aerr2_clock, 
-                       ncol = 1, labels = c('F', 'K'), label_size = 20)
+# Fit clock for all ranges of error
 
-# 10 - Save final plot ====
+# Get existing temporary file
+if(file.exists('temp/age_error_model.rds')) {
+  aerr_out <- readRDS('temp/age_error_model.rds')
+} else {
+  aerr_out <- data.frame()
+}
+if(nrow(aerr_out) == 0) it <- 0 else it <- max(aerr_out$iteration)
+# Start loop
+repeat {
+  it <- it + 1
+  print(it)
+  # Load temp file if number of first iteration exceeds number of times the 
+  # function should be run
+  if(it > n_times) {
+    warning('Number of iterations exceeds number of times the function should
+            be run; returning the accuracy data')
+    return(aerr_out)
+  }
+  # Fit clocks
+  for(i in aerr_seq) {
+    train_i <- train_aerr_dat %>%
+      mutate(age = ifelse(age > 5, age + round(rnorm(1, 0, i)), age))
+    clock_i <- fitClock(train_i, test_aerr_dat, '#ff6666')
+    row_i <- data.frame(iteration = it, error = i, 
+                        mae = clock_i$mae, r2 = clock_i$rsq)
+    aerr_out <- bind_rows(aerr_out, row_i)
+  }
+  # Save temp file with latest iteration in case the loop must be interrupted
+  saveRDS(error_table, 'temp/age_error_model.rds')
+  if(it == n_times) break
+}
 
-# Join  all panels into single figure
-plot_grid(unb_grid, pop_grid, sex_grid, tis_grid, age_grid, aerr_grid, 
-          nrow = 1, rel_widths = c(2, 1, 1, 1, 1, 1))
+# Fit clock without error
+reg_clock <- fitClock(train_aerr_dat, test_aerr_dat, '#ff6666')
+
+reg_clock_mets <- data.frame(metric = c('mae', 'r2'), 
+                             accuracy = c(reg_clock$mae, reg_clock$rsq)) %>%
+  mutate(metric_f = factor(metric, labels = c("Median absolute error", 
+                                              "Pearson's correlation (r)")))
+
+# 8 - Plot accuracy measures, add to list ====
+
+# List of bias variables for plotting
+var_list <- c('pops', 'sex', 'tissue', 'age', 'aerr')
+plt_list <- list()
+for(i in var_list) {
+  # Get correct data
+  dat <- get(paste0(i, '_out'))
+  # If working with age error data, temporarily rename the "error" variable
+  # for plotting
+  if(i == 'aerr') dat <- rename(dat, 'overlap' = error)
+  # Plot
+  plt <- dat %>% 
+    pivot_longer(cols = c(mae, r2), names_to = 'metric', values_to = 'accuracy') %>%
+    mutate(metric_f = factor(metric, 
+                             labels = c("Median absolute error", 
+                                        "Pearson's correlation (r)"))) %>%
+    ggplot(aes(x = factor(overlap), y = accuracy, colour = metric, fill = metric)) + 
+    geom_boxplot() +
+    scale_fill_manual(values = c('#0a75ad50', '#ad420a50')) +
+    scale_colour_manual(values = c('#0a75ad', '#ad420a')) +
+    facet_wrap(~metric_f, scales = 'free') +
+    theme(panel.background = element_rect(fill = 'white', colour = 'black', linewidth = 1),
+          plot.margin = unit(c(0.5, 0.5, 0.75, 0.75), 'cm'),
+          panel.grid = element_blank(),
+          axis.title.x = element_text(colour = 'black', size = 15, vjust = -3),
+          axis.title.y = element_text(colour = 'black', size = 15, vjust = 5),
+          axis.text = element_text(colour = 'black', size = 15),
+          strip.text = element_text(colour = 'black', size = 15),
+          strip.background = element_rect(fill = 'white', colour = 'white'),
+          legend.position = 'none')
+  # Add axis labels; if age error plot, add a line for the regular lasso plot
+  if(i == 'aerr') {
+    plt <- plt + 
+      geom_hline(data = reg_clock_mets, 
+                 aes(yintercept = accuracy), linetype = 'dashed') +
+      labs(x = 'Standard deviation aging error (years)', y = 'Accuracy')
+  } else {
+    plt <- plt +
+      labs(x = 'Proportion overlap training and testing data', y = 'Accuracy')
+  }
+  plt_list[[i]] <- plt
+}
+
+# 9 - Make panel plot and save ====
+
+# Plot panels
+plot_grid(plotlist = plt_list, ncol = 1, labels = LETTERS[1:5], label_size = 20)
 
 # Save
-ggsave('fig2.tiff', plot = last_plot(), 
-       device = 'tiff', path = 'figures/', dpi = 300, height = 15, width = 48, units = 'cm', bg = 'white')
+ggsave('fig1.tiff', plot = last_plot(), 
+       device = 'tiff', path = 'figures/', dpi = 300, height = 48, width = 24, units = 'cm', bg = 'white')
 
 
