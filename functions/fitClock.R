4c53ed748e8ce9bc1f0586e774a22d147f9dbd09
ljnewediuk <j.newedi@gmail.com>
2025-02-06 14:55:16 -0600

    add option for not making plots

--- functions/fitClock.R
+++ functions/fitClock.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(glmnet)
 
 fitClock <- function(train_df, test_df, id_var = 'chip.ID.loc',
-                     pt_col = '#000000', pt_cat = NULL) {
+                     pt_col = '#000000', pt_cat = NULL, mets_only = F) {
   # Get matrix of betas for training data
   train_m <- train_df %>%
     # Make chip positions rownames
@@ -48,44 +48,49 @@ fitClock <- function(train_df, test_df, id_var = 'chip.ID.loc',
   
   # Calculate mae 
   mae <- as.numeric(ie2misc::mae(age_df$agePredict, age_df$age))
+  # Correlation
+  cor <- as.numeric(cor.test(age_df$agePredict, age_df$age)$estimate)
   # And R2
-  rsq <- as.numeric(cor.test(age_df$agePredict, age_df$age)$estimate)
+  rsq <- summary(lm(agePredict ~ age, data = age_df))$r.squared
   
-  # Plot
-  plt <- ggplot(age_df, aes(x = age, y = agePredict)) + 
-    geom_abline(intercept = 0, slope = 1, linetype = 'dashed', linewidth = 1) +
-    theme(panel.background = element_rect(fill = 'white', 
-                                          colour = 'black',
-                                          linewidth = 1),
-          plot.margin = unit(c(0.5, 0.5, 0.75, 0.75), 'cm'),
-          panel.grid = element_blank(),
-          axis.title.x = element_text(colour = 'black', size = 15, vjust = -3),
-          axis.title.y = element_text(colour = 'black', size = 15, vjust = 5),
-          axis.text = element_text(colour = 'black', size = 15),
-          legend.position = 'inside',
-          legend.position.inside = c(.15,.85),
-          legend.title = element_blank(),
-          legend.text = element_text(colour = 'black', size = 15),
-          legend.key = element_rect(colour = 'black', size = 0.5)) +
-    xlim(-2, 30) + ylim(-2, 30) +
-    labs(x = 'Chronological age', y = 'Epigenetic age')
-  
-  # Add colour for plot with single colour
-  if(length(pt_col) == 1) {
-    col_plt <- plt +
-      geom_point(size = 3, colour = pt_col) 
+  if(isFALSE(mets_only)) {
+    # Plot
+    plt <- ggplot(age_df, aes(x = age, y = agePredict)) + 
+      geom_abline(intercept = 0, slope = 1, linetype = 'dashed', linewidth = 1) +
+      theme(panel.background = element_rect(fill = 'white', 
+                                            colour = 'black',
+                                            linewidth = 1),
+            plot.margin = unit(c(0.5, 0.5, 0.75, 0.75), 'cm'),
+            panel.grid = element_blank(),
+            axis.title.x = element_text(colour = 'black', size = 15, vjust = -3),
+            axis.title.y = element_text(colour = 'black', size = 15, vjust = 5),
+            axis.text = element_text(colour = 'black', size = 15),
+            legend.position = 'inside',
+            legend.position.inside = c(.15,.85),
+            legend.title = element_blank(),
+            legend.text = element_text(colour = 'black', size = 15),
+            legend.key = element_rect(colour = 'black', size = 0.5)) +
+      xlim(-2, 30) + ylim(-2, 30) +
+      labs(x = 'Chronological age', y = 'Epigenetic age')
+    
+    # Add colour for plot with single colour
+    if(length(pt_col) == 1) {
+      col_plt <- plt +
+        geom_point(size = 3, colour = pt_col) 
+    }
+    
+    # Add colour for plot with colour by group
+    if(length(pt_col) > 1) {
+      col_plt <- plt +
+        geom_point(size = 3, aes(colour = get(pt_cat))) +
+        scale_colour_manual(values = pt_col)
+    }
+    
+    # Combine a list with plot, age data, and accuracy measures
+    age_list <- list(data = age_df, plot = col_plt, mae = mae, cor = cor, rsq = rsq)
+  } else {
+    age_list <- list(mae = mae, cor = cor, rsq = rsq)
   }
-  
-  # Add colour for plot with colour by group
-  if(length(pt_col) > 1) {
-    col_plt <- plt +
-      geom_point(size = 3, aes(colour = get(pt_cat))) +
-      scale_colour_manual(values = pt_col)
-  }
-  
-  # Combine a list with plot, age data, and accuracy measures
-  age_list <- list(data = age_df, plot = col_plt, mae = mae, rsq = rsq)
-  
   return(age_list)
   
 }
